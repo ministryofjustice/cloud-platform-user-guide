b0ad553ac7a58434c22deb1d473d44f0878b1ece
David Salgado <david@digitalronin.com>
2020-07-06 11:36:15 +0100

    Add a publishing workflow and script
    This should compile the user guide and push to the 
    publishing repo. 
    
    For now, the branch, docker image and the final 
    domain name are set to temporary values. Once I am
    confident everything is working, I'll change to
    the real values.

---
+++ bin/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# PUBLISHING_GIT_TOKEN is a secret in the source repository. It should contain a github personal access token
+# with `public_repo` scope, and MoJ SSO enabled.
+REPOSITORY_PATH="https://${PUBLISHING_GIT_TOKEN}@github.com/$GITHUB_PAGES_REPO_AUTHOR/$GITHUB_PAGES_REPO_NAME.git"
+
+echo "Checkout the publish repo"
+git clone $REPOSITORY_PATH
+
+echo "Add compiled files"
+cp -R docs/* $GITHUB_PAGES_REPO_NAME
+
+cd $GITHUB_PAGES_REPO_NAME
+
+echo "Push the changes"
+git config --global user.email "${GITHUB_PAGES_REPO_AUTHOR_EMAIL}"
+git config --global user.name "${GITHUB_PAGES_REPO_AUTHOR}"
+git add .
+git commit -m "Published at $(date)"
+git push
+
+echo "Publishing complete"
