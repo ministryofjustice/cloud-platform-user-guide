74091b300d6ff795aa49939a8eda626d191e5865
David Salgado <david@digitalronin.com>
2020-07-06 13:46:15 +0100

    Add 'pipefail' to the publish script
    We want the script to fail if any of the sub-commands is not
    successful

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# This script is called by .github/workflows/publish.yml after compiling the source documents into
+# HTML files in the `docs` directory.
+#
+# It adds and pushes the compiled files to a 'publishing' repo, where they are served as a website via
+# github pages.
+
+set -euo pipefail
+
 # PUBLISHING_GIT_TOKEN is a secret in the source repository. It should contain a github personal access token
 # with `public_repo` scope, and MoJ SSO enabled.
 REPOSITORY_PATH="https://${PUBLISHING_GIT_TOKEN}@github.com/$GITHUB_PAGES_REPO_OWNER/$GITHUB_PAGES_REPO_NAME.git"
