85237cc6956442d41e84f72bbd3051b27985ca73
David Salgado <david@digitalronin.com>
2020-08-27 12:04:13 +0100

    Suppress noisy warnings during the check phase
    These warnings add no value, and make the link-checker output impossible
    to read.

--- bin/check.sh
+++ bin/check.sh
@@ -8,6 +8,13 @@
 
 set -euo pipefail
 
-# Check for internal and external broken links
-bundle exec htmlproofer ./docs --http-status-ignore 0,429 --allow-hash-href --url-swap "https?\:\/\/user-guide\.cloud-platform\.service\.justice\.gov\.uk:" ./docs
+# Check for internal and external broken links The 'grep -v' suppresses
+# annoying warnings due to some of the gem dependencies using functions that
+# have been deprecated in current versions of ruby.
+bundle exec htmlproofer 2>&1 \
+  --http-status-ignore 0,429 \
+  --allow-hash-href \
+  --url-swap "https?\:\/\/user-guide\.cloud-platform\.service\.justice\.gov\.uk:" \
+  ./docs | grep -v "warning: URI.unescape is obsolete"
+
 
